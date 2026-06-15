0d1c615afcb55c119b0343858048a3ce14a0f41a
geniuspc <sensei.prihodko@gmail.com>
2026-06-15 22:56:05 +0300

    Implemented vol3-service logic and functions

---
+++ models.go
@@ -0,0 +1,25 @@
+package main
+
+type ArtifactFiles struct {
+	Memory string   `json:"memory"`
+	Logs   []string `json:"logs"`
+}
+
+type EvidenceMsg struct {
+	CaseID        string        `json:"case_id"`
+	ArtifactType  string        `json:"artifact_type"`
+	MountPath     string        `json:"mount_path"`
+	OsHint        string        `json:"os_hint"`
+	ByteSize      int64         `json:"size_bytes"`
+	Vol3Readiness bool          `json:"vol3_ready"`
+	CheckSum      string        `json:"checksum"`
+	Files         ArtifactFiles `json:"files"`
+	Timestamp     time.Time     `json:"ts"`
+}
+
+type Task func()
+
+type ThreadPoolExecutor struct {
+	taskQueue chan Task
+	wg        sync.WaitGroup
+}
